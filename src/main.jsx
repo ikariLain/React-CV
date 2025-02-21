5a8b087f8dbf9bd870ff7d5fb2091c0e1e6c920e
ikariLain <matheus-torrico@outlook.com>
2025-02-21 16:18:13 +0100

    Fix main

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,9 @@ import App from './App'
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root')
-)
-
+  document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename='/React-CV/'>
     <App />
-  </React.StrictMode>
-)
+  </BrowserRouter>
+);
