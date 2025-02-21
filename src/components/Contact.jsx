215ca1e9ab45a2c931901ee10425bcd0ca0e2419
ikariLain <matheus-torrico@outlook.com>
2025-02-21 14:56:01 +0100

    Fix contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ImgPainting from "../assets/Sea_painting.jpg"
+import "../styles.css"
 
 const Contact = () => {
-  
     return (
-      
       <div class="contact-container">
       <h1>Kontakta Mig</h1>
-      <img src={ImgPainting} class="painting">
+      <img src={ImgPainting} class="painting"/>
       
       <div class="contact-info">
 
           <div class="contact-item">
               <i class="fab fa-linkedin"></i>
-              <h3>LinkedIn</h3>
+              <h3>Linkedin</h3>
               <p><a href="https://www.linkedin.com/in/matheus-torrico-076364182/" target="_blank">LinkedIn Profil</a></p>
           </div>
           
           <div class="contact-item">
               <i class="fab fa-github"></i>
-              <h3>GitHub</h3>
+              <h3>Github</h3>
               <p><a href="https://www.linkedin.com/in/matheus-torrico-076364182/" target="_blank">GitHub Profil</a></p>
           </div>
       </div>
   </div>
-    );
-};
+    )
+}
 
 export default Contact
